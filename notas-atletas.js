88924ad5d50fe64290a6ad6e297020e855d10f51
euaraujorlk7-cyber <euaraujorlk7@gmail.com>
2025-10-23 23:04:07 -0400

    notas-atletas.js

---
+++ notas-atletas.js
@@ -0,0 +1,38 @@
+let atletas = [
+  {
+    nome: "Cesar Abascal",
+    notas: [10, 9.34, 8.42, 10, 7.88]
+  },
+  {
+    nome: "Fernando Puntel",
+    notas: [8, 10, 10, 7, 9.33]
+  },
+  {
+    nome: "Daiane Jelinsky",
+    notas: [7, 10, 9.5, 9.5, 8]
+  },
+  {
+    nome: "Bruno Castro",
+    notas: [10, 10, 10, 9, 9.5]
+  }
+];
+
+// === Cálculo das médias e exibição ===
+for (let i = 0; i < atletas.length; i++) {
+  let atleta = atletas[i];
+  let notas = atleta.notas.sort((a, b) => a - b);
+  
+  // Remove a menor e a maior nota
+  let notasComputadas = notas.slice(1, 4);
+  
+  // Calcula a média das 3 notas do meio
+  let soma = 0;
+  notasComputadas.forEach(nota => soma += nota);
+  let media = soma / notasComputadas.length;
+
+  // Exibe o resultado
+  console.log(`Atleta: ${atleta.nome}`);
+  console.log(`Notas Obtidas: ${atleta.notas.join(",")}`);
+  console.log(`Média Válida: ${media}`);
+  console.log(""); // linha em branco
+}
